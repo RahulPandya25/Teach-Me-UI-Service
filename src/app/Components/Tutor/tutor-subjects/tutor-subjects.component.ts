b2db6bd1f1ea62fc297e54bec04abc4cdcf9c180
Rahul Pandya <rahul25796@gmail.com>
2020-03-14 02:31:27 -0400

    updated tutor screen

--- src/app/Components/Tutor/tutor-subjects/tutor-subjects.component.ts
+++ src/app/Components/Tutor/tutor-subjects/tutor-subjects.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-tutor-subjects',
-  templateUrl: './tutor-subjects.component.html',
-  styleUrls: ['./tutor-subjects.component.scss']
+  selector: "app-tutor-subjects",
+  templateUrl: "./tutor-subjects.component.html",
+  styleUrls: ["./tutor-subjects.component.scss"]
 })
 export class TutorSubjectsComponent implements OnInit {
+  subjects = [
+    { name: "Java", noOfTests: 10 },
+    { name: "PHP", noOfTests: 10 },
+    { name: "Python", noOfTests: 10 },
+    { name: ".Net", noOfTests: 10 }
+  ];
+  pageTitle = "Subjects";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
