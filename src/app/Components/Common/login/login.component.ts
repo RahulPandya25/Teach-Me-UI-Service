12ebeba81a5fe8591a10f9912ca7da310f8af46e
Rahul Pandya <rahul25796@gmail.com>
2020-03-20 21:28:17 -0400

    updated login module

--- src/app/Components/Common/login/login.component.ts
+++ src/app/Components/Common/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { FormGroup, FormControl } from "@angular/forms";
 import { ConstantsService } from "src/app/Services/constants.service";
+import { LoginService } from "src/app/Services/login.service";
 
 @Component({
   selector: "app-login",
@@ -9,20 +11,40 @@ import { ConstantsService } from "src/app/Services/constants.service";
 })
 export class LoginComponent implements OnInit {
   showLoginForm;
+  user;
+  loginForm;
+  loginFormGroup = new FormGroup({
+    email: new FormControl(""),
+    password: new FormControl("")
+    // userType: new FormControl("")
+  });
+  registerForm;
+  registerFormGroup = new FormGroup({
+    email: new FormControl(""),
+    name: new FormControl(""),
+    contact: new FormControl(""),
+    password: new FormControl(""),
+    userType: new FormControl("")
+  });
+
   formTitle;
 
-  email;
-  pwd;
-  uType;
-  name;
-  contact;
+  submitForm(form) {
+    console.log(form.value);
 
-  submitForm() {
-    if (this.showLoginForm) {
-      localStorage.setItem("userType", this.uType);
-      if (this.uType === "Student") this.router.navigateByUrl("/student");
-      if (this.uType === "Tutor") this.router.navigateByUrl("/tutor");
-    }
+    this.loginService
+      .systemIn(form.value, this.showLoginForm)
+      .subscribe(response => {
+        if (response && response !== null) {
+          this.user = Object.assign(response);
+          localStorage.setItem("user", JSON.stringify(this.user));
+
+          if (this.user.userType === "STUDENT")
+            this.router.navigateByUrl("/student");
+          if (this.user.userType === "TUTOR")
+            this.router.navigateByUrl("/tutor");
+        }
+      });
   }
 
   switchForm() {
@@ -31,7 +53,7 @@ export class LoginComponent implements OnInit {
     else this.formTitle = "Register";
   }
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private loginService: LoginService) {}
 
   ngOnInit(): void {
     this.showLoginForm = true;
