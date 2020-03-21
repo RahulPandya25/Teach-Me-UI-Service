e1a233df3d28a7b62779d83abec4757f8e389644
Rahul Pandya <rahul25796@gmail.com>
2020-03-20 23:07:07 -0400

    updated routing from student test page

--- src/app/Components/Student/student-tests/student-tests.component.ts
+++ src/app/Components/Student/student-tests/student-tests.component.ts
@@ -10,6 +10,7 @@ import { TestService } from "src/app/Services/test.service";
 export class StudentTestsComponent implements OnInit {
   subject;
   subjectId;
+  user;
   pageTitle = "Tests";
   loggedInUser = { userId: 1 };
 
@@ -17,16 +18,31 @@ export class StudentTestsComponent implements OnInit {
     this.testService
       .isTestAttemted(testId, this.loggedInUser.userId)
       .subscribe(response => {
-        console.log(response);
+        if (response === false) {
+          this.router.navigate(["student/tests/takeTest"], {
+            queryParams: {
+              testId: testId
+            }
+          });
+        } else if (response === true) {
+          this.router.navigate(["/report"], {
+            queryParams: {
+              testId: testId
+            }
+          });
+        }
       });
   }
 
   constructor(
     private route: ActivatedRoute,
-    private testService: TestService
+    private testService: TestService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem("user"));
+
     this.route.queryParams.subscribe(params => {
       this.subjectId = params["subjectId"];
     });
